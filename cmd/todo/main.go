d1962d1d043fa71ca69fc764d73b4d4436beb212
asherbabu <asherbabu65@gmail.com>
2024-06-10 14:29:50 +0530

    add and complete function added to main

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -14,7 +14,8 @@ const(
 
 func main(){
 	
-	add := flag.Bool("add", false, "add a new tdod")
+	add := flag.Bool("add", false, "add a new todo")
+	complete := flag.Int("complete", 0, "mark a todo as completed")
 
 	flag.Parse()
 
@@ -33,9 +34,22 @@ func main(){
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
 		}
+	case complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
 	default:
 		fmt.Fprintln(os.Stdout, "invalid command")
 		os.Exit(0)
 	}
 
 }
+
