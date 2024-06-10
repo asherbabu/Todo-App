cbbf74906a03848bce47160669719181b513c5e0
asherbabu <asherbabu65@gmail.com>
2024-06-10 15:02:15 +0530

    input from user function added

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -71,3 +71,24 @@ func main(){
 
 }
 
+func getInput(r io.Reader, args ...string) (string, error){
+	if len(args) > 0{
+		return strings.Join(args, " "), nil
+	}
+
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	text := scanner.Text()
+
+	if len(text) == 0{
+		return "", erros.New("empty todo is not allowed")
+	}
+
+	return text, nil
+
+}
+
