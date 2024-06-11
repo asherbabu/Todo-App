3c75be683c75d77e9a08bcfec110e7d12c58a051
asherbabu <asherbabu65@gmail.com>
2024-06-11 14:13:24 +0530

    changed print fucntions for tablle header and cells

--- todo.go
+++ todo.go
@@ -3,10 +3,12 @@ package todoapp
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"time"
-	"fmt"
+
+	"github.com/alexeyco/simpletable"
 )
 
 type item struct {
@@ -83,8 +85,27 @@ func (t *Todos) Store(filename string) error {
 }
 
 func (t *Todos) Print(){
-	for i, item := range *t {
-		i++
-		fmt.Printf("%d - %s\n", i, item.Task)
+	table := simpletable.New()
+
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "#"},
+			{Align: simpletable.AlignCenter, Text: "Task"},
+			{Align: simpletable.AlignCenter, Text: "Done?"},
+			{Align: simpletable.AlignRight, Text: "CreatedAt"},
+			{Align: simpletable.AlignRight, Text: "CompletedAt"},
+		},
+	}
+
+	var cells [][]*simpletable.Cell
+	for idx, item := range *t {
+		idx++
+		cells = append(cells, *&[]*simpletable.Cell{
+			{Text: fmt.Sprintf("%d", idx)},
+			{Text: item.Task},
+			{Text: fmt.Sprintf("%t", item.Done)},
+			{Text: item.CreatedAt.Format(time.RFC822)},
+			{Text: item.CompletedAt.Format(time.RFC822)},
+		})
 	}
 }
